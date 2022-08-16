6e384f6ff36cf51990cadf7010d33a85066a4b3b
JuneAekkalak <aekkalak.ju@gmail.com>
2022-08-16 18:08:36 +0700

    Other add

---
+++ TestProject/src/CalRect.java
@@ -0,0 +1,9 @@
+
+public class CalRect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestProject/src/DisplayMessage.java
@@ -0,0 +1,14 @@
+
+public class DisplayMessage {
+
+	public static void main(String[] args) {
+		System.out.println("**************************************");
+		System.out.println("*       Programming Assignment 1     *");
+		System.out.println("*         Java Programming           *");
+		System.out.println("*    Author:  Your Name-Surname      *");
+		System.out.println("*         Date:  Today               *");
+		System.out.println("**************************************");
+
+	}
+
+}
